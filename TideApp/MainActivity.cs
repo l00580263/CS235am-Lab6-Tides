fe924459c05e719dcf6bf81aceb737b30e9e1cb4
unknown <you@example.comdedorec2@my.lanecc.edu>
2018-11-03 12:02:21 -0700

    List works

--- TideApp/MainActivity.cs
+++ TideApp/MainActivity.cs
@@ -3,17 +3,41 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Views;
+using TideTableUsingXmlFile;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace TideApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : ListActivity
     {
+
+
+
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+
+            // read xml
+            XmlTideFileParser noaa = new XmlTideFileParser(Assets.Open("tides_annual.xml"));
+
+            ListAdapter = new SimpleAdapter(this, noaa.TideList, Resource.Layout.rowLayout, 
+                new string[] {XmlTideFileParser.DATE, XmlTideFileParser.HI_LOW, XmlTideFileParser.TIME}, 
+                new int[] {Resource.Id.dateText, Resource.Id.hiLowText, Resource.Id.timeText } );
+
+            ListView.FastScrollEnabled = true;
+        }
+
+
+
+        protected override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            
         }
     }
 }
